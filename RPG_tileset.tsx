391da1ab3eb29b759104c1e724551b95d922c864
Oleg Koshel <oleg.koshel@wandera.com>
2022-07-03 22:10:01 +0100

    PP-BBQ-WINE tsx + png upload

---
+++ RPG_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="RPG_tileset" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <image source="../Tile_sets/Pipoya RPG Tileset 32x32/[Base]BaseChip_pipo.png" width="256" height="4256"/>
+</tileset>
