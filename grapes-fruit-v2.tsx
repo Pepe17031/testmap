33e746730f5361b81c9101fad076e4e46e45f39c
Oleg Koshel <oleg.koshel@wandera.com>
2022-07-04 18:40:08 +0100

    PP-TSX fix tilesets

--- grapes-fruit-v2.tsx
+++ grapes-fruit-v2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.0" name="grapes-fruit-v2" tilewidth="32" tileheight="32" tilecount="108" columns="12">
- <image source="../../../Downloads/grapes-fruit-v2.png" width="405" height="309"/>
+ <image source="tilesets/grapes-fruit-v2.png" width="405" height="309"/>
 </tileset>
